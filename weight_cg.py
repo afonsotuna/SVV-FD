1def6cc57bd21c49cfde747d65a75a6a715238f7
Fernando <f.cortevargas@student.tudelft.nl>
2020-03-05 17:32:50 +0100

    Created the cg and weight excursion files and defined the initial BEM and fuel parameters.

--- weight_cg.py
+++ weight_cg.py
@@ -1,7 +1,6 @@
 # WEIGHT AND CENTRE OF GRAVITY AS FUNCTIONS OF TIME
 
 # INITIAL PARAMETERS
-
 BEM_w = 9165.0
 BEM_arm = 291.65
 BEM_m = BEM_w * BEM_arm
@@ -9,6 +8,12 @@ BEM_m = BEM_w * BEM_arm
 fuel_w = 750.0
 fuel_arm = 287.58
 fuel_m = fuel_w * fuel_arm
+
+# CONVERSION FACTORS
+in_to_m = 2.54E-2
+lbs_to_kg = 0.453592
+
+#
 
 
 
