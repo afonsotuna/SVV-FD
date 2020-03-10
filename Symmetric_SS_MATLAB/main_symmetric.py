a8c0af00a45703c98a6dc560bd1515e86b69726f
afonsotuna <afonso.tuna@gmail.com>
2020-03-10 12:17:47 +0100

    Changed code structure, now reading reference data. Results to be debugged.

--- Symmetric_SS_MATLAB/main_symmetric.py
+++ Symmetric_SS_MATLAB/main_symmetric.py
@@ -4,30 +4,43 @@ import scipy.io
 import numpy as np
 from Symmetric_SS_MATLAB.ss_symmetric import ss_sym
 
-# Flight data imported and SS system computed
-mat = scipy.io.loadmat('flight1_clean.mat')
-flight_data = mat['clean_data']
-sys = ss_sym(m=6324, V=80)  # for blank arguments => rho=1.225, theta_0=0, m=4157.174, V=80
-
 # Define event (from flight test sheet)
-t_lookup = 3020  # time (in seconds) at which event happens
-t_limit = 140  # length of interval (in seconds, multiples of 0.1)
+t_lookup = 3237  # time (in seconds) at which event happens
+t_limit = 120  # length of interval (in seconds, multiples of 0.1)
+block_fuel = 4050  # block fuel (lbs)
+passenger_weight = 695  # sum of passenger weight (kgs)
+
+# Flight data imported and SS system computed
+mat = scipy.io.loadmat('reference_clean.mat')
+flight_data = mat['clean_data']
+
+# Get data location
+index = int((t_lookup - flight_data[0, 47]) / 0.1)
+n_points = int(t_limit / 0.1) + 1
+
+# Obtain correct weight (manoeuvre start) and velocity, get system
+used_fuel = flight_data[index, 13] + flight_data[index, 14]
+mass_event = (block_fuel - used_fuel + 9165) * 0.453592 + passenger_weight
+tas_event = flight_data[index, 41] * 0.514444
+sys = ss_sym(m=mass_event, V=tas_event)
 
 # Obtain correspondent flight data
-index = int((t_lookup - flight_data[0, 48]) / 0.1)
-n_points = int(t_limit / 0.1) + 1
 data_event = np.zeros((n_points, 2))
 for i in range(n_points):
-    data_event[i, 0] = flight_data[index + i, 48] - flight_data[index, 48]
-    data_event[i, 1] = flight_data[index + i, 0] - flight_data[index, 0]  # Outputs: ?? - u / 0 - alpha / ?? - theta / ?? - q
+    data_event[i, 0] = flight_data[index + i, 47] - flight_data[index, 47]
+    data_event[i, 1] = flight_data[index + i, 0] - flight_data[
+        index, 0]  # Outputs: ?? - u / 0 - alpha / ?? - theta / ?? - q
 t1 = data_event[:, 0]
 y1 = data_event[:, 1]
 
 # Obtain impulse response
-t2, y2 = control.impulse_response(sys, T=t1, output=2)  # Outputs: 1 - u / 2 - alpha / 3 - theta / 4 - q
+input_delta_e = flight_data[index:index + n_points, 16]
+# t2, y2 = control.impulse_response(sys, T=t1, output=2)
+t2, out, p2 = control.forced_response(sys, T=t1, U=input_delta_e)
+y2 = out[1, :]  # Outputs: 0 - u / 1 - alpha / 2 - theta / 3 - q
 
-
-# IN DEBUGGING - DON'T TOUCH (currently looking at phugoid for flight 1)
+# IN DEBUGGING - DON'T TOUCH (currently looking at phugoid for reference data)
 plt.plot(t1, y1)
 plt.plot(t2, y2)
+plt.plot(t2, input_delta_e)
 plt.show()
