48f64f6106a8aa3104e562a81477e943ce00a406
Mariano <nano2598.r@gmail.com>
2020-03-26 04:18:00 +0100

    Finally finished the plotting for the CL_CD_curve.py file

--- Data Processing - MATLAB/Trim_curve.py
+++ Data Processing - MATLAB/Trim_curve.py
@@ -15,7 +15,7 @@ d = 0.686     # diameter of engine [m], from online source
 W_s = 60500   # standard weight [N]
 Cm0 = 0.0297
 
-ref = 1
+ref = 0
 # reading data unto pandas dataframe:
 if ref == 1:
     exc_data = 'Post_Flight_Datasheet_Flight_1_DD_12_3_2018.xlsx'
@@ -62,8 +62,13 @@ hp2 = data2['hp'].iloc[1:].to_numpy(dtype=float)  # Putting pressure alt (hp) va
 V_ias2 = data2['IAS'].iloc[1:].to_numpy(dtype=float)    # Putting V_IAS values from the dataframe column to np array
 Tm2 = data2['TAT'].iloc[1:].to_numpy(dtype=float)     # Putting TAT values from the dataframe column to np array
 
-V_e2, T2, M2, Re2 = reduce(hp2, V_ias2, Tm2)
+V_e2, T2, M2, Re_range = reduce(hp2, V_ias2, Tm2)
 F_used = data2['F. used'].iloc[1:].to_numpy(dtype=float)  # Same as what's done above but for fuel used
+
+
+print("Re range is from " + np.format_float_scientific(float(Re_range[0])) + ' to ' + np.format_float_scientific(
+    float(Re_range[1])))
+print('Mach range is from ' + str(round(min(M2), 2)) + ' to ' + str(round(max(M2), 2)))
 
 pax_masses = np.array(Pd.read_excel(exc_data, header=None, usecols='H', skiprows=7, nrows=9))
 init_fuel = xlrd.open_workbook(exc_data).sheet_by_index(0).cell_value(17, 3)
@@ -113,13 +118,14 @@ def inv_f (x, a, b):
 
 popt, pcov = optimize.curve_fit(inv_f, V_e_red2, delta_e_red)
 xlab = ('Reduced velocity (\u1E7C$_e$) [m/s]')
-ylab = ('Reduced deflection ($\delta_{e_{eq}}^*$) [deg]')
-plt.axes(xlim=(50, 200), ylim=(4, -3), xlabel=xlab, ylabel=ylab)
+ylab = ('Reduced elevator deflection ($\delta_{e_{eq}}^*$) [deg]')
+plt.axes(xlim=(60, 180), ylim=(3, -3), xlabel=xlab, ylabel=ylab)
+plt.xticks(np.arange(40, 190, step=20))
 x1 = np.linspace(1, 250, 250)
 plt.plot(x1, inv_f(x1, *popt), 'darkturquoise', label='Inverse square least-squares fit')
 plt.scatter(V_e_red2, delta_e_red, s=70, c='red', marker='x', label='Reduced elevator deflection data')
-plt.plot(x1, (-Cm0/C_m_deltae) * np.ones(250) * 180/np.pi, '-g')
-plt.plot(x1, popt[0] * np.ones(250))
+#plt.plot(x1, popt[0] * np.ones(250))
 plt.grid()
 plt.legend(loc='best')
+plt.savefig('figs/Reduced_Trim_curve')
 plt.show()
